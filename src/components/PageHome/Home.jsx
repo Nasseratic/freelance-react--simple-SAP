ace12fe17d6c7c321402e161a541d3196c9b1844
Mohamed Nasser <mhmeeaad@gmail.com>
2018-01-10 01:36:59 +0200

    Last_last_last changes

--- src/components/PageHome/Home.jsx
+++ src/components/PageHome/Home.jsx
@@ -1,55 +1,70 @@
 import React from 'react';
 import './Home.css';
-import {Clocks} from './mini/Clocks'
-import {Extchange} from './mini/Extchange'
+import { Clocks } from './mini/Clocks'
+import { Extchange } from './mini/Extchange'
 import { About } from './About/About'
 import { Products } from './Products/Products'
 import { NewsBar } from './NewsBar/NewsBar'
 import { Contact } from './Contact/Contact'
+import Carousel from 'nuka-carousel';
 
 
 export class Home extends React.Component {
-    
-    constructor(props){
+
+    constructor(props) {
         super(props);
         this.state = {};
         this.handleToggleClick = this.handleToggleClick.bind(this);
-        
+
     }
     handleToggleClick() {
         this.setState(prevState => ({
         }));
-      }
+    }
     render() {
+        function ImgSec(bg,text) {
+            return (
+                <section className="hero is-primary is-medium">
+                    <div className="hero-body" style={{ backgroundImage: "url('" + bg + "')", backgroundSize: 'cover', backgroundPosition: 'center' }} >
+                        <div className="container">
+                            
+                            <h2 className="title is-size-3 has-text-centered is-uppercase" style={{background:'rgba(71, 74, 78, 0.726)' , padding:20}} >
+                                {text}
+                            </h2>
+                        </div>
+                    </div>
+                </section>
+            );
+        }
         return (
             <div>
-                <section className="hero hero-home is-fullheight   is-bold" id="home">
-                <br/>
-                <br/>
-                <br/>
-                <Clocks/>
-                <Extchange/>
-                <NewsBar/>
-                    <div className="hero-body">                
-                        <div className="container  has-text-centered">
-
-                            <h1 className="title has-text-white is-size-1 is-uppercase">
-                            Hashem
-                            </h1>
-                            <h2 className="subtitle has-text-grey-lighter is-size-3 is-uppercase">
-                            For import and export 
-                            </h2>
-
-                        </div>
-                        
+                <section className="hero hero-home is-fullheight  is-bold" id="home">
+                    <br />
+                    <br />
+                    <br />
+                    <Clocks />
+                    <Extchange />
+                    <NewsBar />
+                    <div className="card">
+                    <Carousel>
+                    {ImgSec('../../slider/slide-1.png', 'Hashem for import and export')}
+                    {ImgSec('../../slider/slide-2.png', 'In Cooperation With Our Business Partners, We Strive Towards Mutual Benefit As The Basic Of Long-Term Relations.')}
+                    {ImgSec('../../slider/slide-3.png', 'Continuous Grow The Plays An Important Role In Developing And Safe Guarding The Company For The Long Term.')}
+                    {ImgSec('../../slider/slide-4.png', 'We Promote The Further Training Of Our Staff To Enhance Their Performance.')}
+                    {ImgSec('../../slider/slide-5.png', 'We Welcome Constructive Suggestions As An Incentive For Improvement.')}
+                    {ImgSec('../../slider/slide-6.png', 'We Actively Work To Meet Increasing Customers Requirements And Shape Market Changes.')}
+                    {ImgSec('../../slider/slide-7.png', 'In Cooperation With Our Business Partners, We Strive Towards Mutual Benefit As The Basic Of Long-Term Relations.')}
+                    </Carousel>
                     </div>
-                    <span className='icon-scroll'> </span>
-                    
                 </section>
-                <About/>
-                <Products/>
-                <Contact/>
+                
+
+                <About />
+                <Products />
+                <Contact />
             </div>
         );
     }
 }
+
+//  <span className='icon-scroll'> </span>
