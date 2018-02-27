f91dc051bec7c466735a52d1acbfe3d0b1a1997b
Mohamed Nasser <mhmeeaad@gmail.com>
2018-02-27 23:21:16 +0200

    dynamic contact info ,  changing some images ..

--- src/components/PageHome/Contact/Contact.jsx
+++ src/components/PageHome/Contact/Contact.jsx
@@ -1,8 +1,34 @@
 import React from 'react';
 import './Contact.css';
+import firebase from 'firebase';
 
 export class Contact extends React.Component {
+  constructor(props) {
+    super();
+    this.state= { contact : {} }
+  }
 
+  componentDidMount(){
+    var config = {
+      apiKey: "AIzaSyDPuwC9d-YJ5XtRpOCQGtbLRjzGSJrwcp8",
+      authDomain: "simple-api-1545.firebaseapp.com",
+      databaseURL: "https://simple-api-1545.firebaseio.com",
+      };
+      try {
+          firebase.initializeApp(config)
+      } catch (err) {
+          // we skip the "already exists" message which is
+          // not an actual error when we're hot-reloading
+          if (!/already exists/.test(err.message)) {
+          console.error('Firebase initialization error', err.stack)
+          }
+      }
+      
+      var database = firebase.database();
+      firebase.database().ref('contact').once('value').then((res) => {
+          this.setState({contact : res.val()});
+      });
+  }
   render() {
     return (
       <div id="contact" className="contact">
@@ -15,26 +41,26 @@ export class Contact extends React.Component {
               <div className="field">
                 <label className="label">PHONE</label>
                 <div className="control">
-                  <input className="input is-size-5" value="+20010000000" type="text" placeholder="" />
+                  <input className="input is-size-5" value={ this.state.contact.phone || ".........."} type="text" placeholder="" />
                 </div>
               </div>
               <div className="field">
                 <label className="label">FAX</label>
                 <div className="control">
-                  <input className="input is-size-5" value="+2051477855" type="text" />
+                  <input className="input is-size-5" value={ this.state.contact.fax ||".........."} type="text" />
                 </div>
               </div>
               <div className="field">
                 <label className="label">EMAIL</label>
                 <div className="control">
-                  <input className="input is-size-5" value="info@hessham.com" type="email" placeholder="" />
+                  <input className="input is-size-5" value={ this.state.contact.email || ".........." } type="email" placeholder="" />
                 </div>
               </div>
               
               <div className="field">
                 <label className="label">ADDRESS</label>
                 <div className="control">
-                  <input className="input is-size-5" value="Address" type="text" placeholder="" />
+                  <input className="input is-size-5" value={ this.state.contact.address ||".........."} type="text" placeholder="" />
                 </div>
               </div>
               {/* <div className="field">
@@ -100,8 +126,9 @@ export class Contact extends React.Component {
               </div> */}
               {/* <a className="button submitButton is-medium">SUBMIT <i className="fa fa-send"></i> </a> */}
             </div>
-            <div className="column card" style={{ background:"url('http://alqatbiglobal.com/wp-content/uploads/2018/01/contact.jpg')", backgroundSize: 'cover' }}>
-              
+            <div className="column card">
+            <div style={{ background:"url('http://alqatbiglobal.com/wp-content/uploads/2018/01/contact.jpg')", backgroundSize: 'cover', height:"100%" , width:"100%" }}>
+            </div>
             </div>
           </div>
           
--- src/components/PageProduct/Product.jsx
+++ src/components/PageProduct/Product.jsx
@@ -1283,12 +1283,12 @@ contents['Citrus'] = (
 
 contents['De-Ice Salt'] = (
     <body>
-        <div className="card">
-        <video width="55%" height="340" controls controlsList="nodownload noremote foobar">
+        <video className="card" style={{width:"55%", height: "400px", margin:"0 auto"}} controls controlsList="nodownload noremote foobar">
   <source src="../../salt.mp4"  type="video/mp4"/>
 </video>
-</div>
-<img width="450" height="340" style={{float:'right'}} src="../../catimg/remove.jpg"/>
+{/* <img width="450" height="340" style={{float:'right'}} src=""/> */}
+
+<div style={{ width:"38%", float:"right" , height:"400px",  background:"url(../../catimg/remove.jpg)", backgroundPosition:"center"  ,backgroundSize:"cover" , minWidth:"300px"}} ></div>
         <p> We able to produce and supply over 2 million ton per years , we able enter any tender for salt around the globe , we looking for reduce invoice  of cleaning roads from the snow by supply super quality salt making roads very safe , with best prices on time without any delay .
     De-icing Salt (Sodium chloride) is the most widely used de-icing agent in Europe , canada and usa and throughout the world due to its huge advantages over the alternatives.</p>
         <ul> It is the ideal de-icing material because:</ul>
